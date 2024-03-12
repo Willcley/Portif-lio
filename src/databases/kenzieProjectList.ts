2df010be6074bc31b2989ea73a73703816d43bd1
Willcley Melo <willcley2003@gmail.com>
2024-03-12 11:43:45 -0300

    fix: change projects database to typescript

---
+++ src/databases/kenzieProjectList.ts
@@ -0,0 +1,310 @@
+import { TProject } from "@/interfaces/projects.interfaces";
+
+export const kenzieProjectList: TProject[] = [
+    // {
+    //     img: null,
+    //     isNew: false,
+    //     title: null,
+    //     type: "front-end",
+    //     kenzie: true,
+    //     framework: null,
+    //     techList: [
+    //     ],
+    //     description: null,
+    //     finishedAt: null,
+    //     linkGithub: null,
+    //     publicGithub: false,
+    //     linkAppliaction: null,
+    //     online: false,
+    // },
+    {
+        img: "gerenciadorDeContatosApp.png",
+        isNew: false,
+        title: "Gerenciador de Contatos (App)",
+        type: "front-end",
+        kenzie: true,
+        framework: "React.js",
+        techList: [
+            "React.js",
+            "Sass",
+            "Html",
+            "Zod",
+            "Axios",
+        ],
+        description: `
+            Um site de gerenciamento de contatos.
+            \n
+            Esse foi meu primeiro projeto full stack, juntamente da api de gerenciamento de contatos.
+            \n
+            Desenvolvi ele com o intuito de juntar meus conhecimentos de front-end e de back-end.
+        `,
+        finishedAt: null,
+        linkGithub: "https://github.com/Kenzie-Academy-Brasil-Developers/m6-desafio-fullstack-willcley_frontend",
+        publicGithub: true,
+        linkAppliaction: "https://gerenciador-de-contatos-nu.vercel.app/",
+        online: true,
+    },
+    {
+        img: "gerenciadorDeContatosApi.png",
+        isNew: false,
+        title: "Gerenciador de Contatos (Api)",
+        type: "back-end",
+        kenzie: true,
+        framework: "Nest.js",
+        techList: [
+            "Nest.js",
+            "Express",
+            "Typescript",
+            "Zod",
+            "TypeORM",
+            "PostgreSQL",
+            "Node.js",
+        ],
+        description: `
+            Meu primeiro projeto de back-end utilizando o Nest.js, em que simula uma api de gerenciamento de contatos.
+            \n
+            Nele pratiquei meus conhecimentos em back-end, com base no Nest.js.
+        `,
+        finishedAt: "07/02/2024",
+        linkGithub: "https://github.com/Kenzie-Academy-Brasil-Developers/m6-desafio-fullstack-willcley_backend",
+        publicGithub: true,
+        linkAppliaction: "https://github.com/Kenzie-Academy-Brasil-Developers/m6-desafio-fullstack-willcley_backend",
+        online: true,
+    },
+    // {
+    //     img: "kanvas.png",
+    //     isNew: false,
+    //     title: "Kanvas",
+    //     type: "back-end",
+    //     kenzie: true,
+    //     framework: "Django",
+    //     techList: [
+    //         "Django",
+    //         "Python",
+    //         "PostgreSQL",
+    //     ],
+    //     description: `
+    //         Back-end
+    //         (Privado)
+    //     `,
+    //     finishedAt: "07/01/2024",
+    //     linkGithub: "https://github.com/Willcley/m5-projeto-final-kanvas_willcley",
+    //     publicGithub: true,
+    //     linkAppliaction: "https://django-app-kanvas.onrender.com/api/docs/",
+    //     online: true,
+    // },
+    // {
+    //     img: null,
+    //     isNew: false,
+    //     title: "BandKamp Generic View",
+    //     type: "back-end",
+    //     kenzie: true,
+    //     framework: "Django",
+    //     techList: [
+    //         "Django",
+    //         "Python",
+    //         "PostgreSQL",
+    //     ],
+    //     description: `
+    //         Back-end
+    //         (Privado)
+    //     `,
+    //     finishedAt: "14/12/2023",
+    //     linkGithub: "https://github.com/Willcley/m5-bandkamp-generic-view_willcley",
+    //     publicGithub: true,
+    //     linkAppliaction: null,
+    //     online: false,
+    // },
+    // {
+    //     img: null,
+    //     isNew: false,
+    //     title: "Kenzie Buster",
+    //     type: "back-end",
+    //     kenzie: true,
+    //     framework: "Django",
+    //     techList: [
+    //         "Django",
+    //         "Python",
+    //         "PostgreSQL",
+    //     ],
+    //     description: `
+    //         Back-end
+    //         (Privado)
+    //     `,
+    //     finishedAt: "09/12/2023",
+    //     linkGithub: "https://github.com/Willcley/m5-kenzie-buster_willcley",
+    //     publicGithub: true,
+    //     linkAppliaction: null,
+    //     online: false,
+    // },
+    // {
+    //     img: null,
+    //     isNew: false,
+    //     title: "Pet Kare",
+    //     type: "back-end",
+    //     kenzie: true,
+    //     framework: "Django",
+    //     techList: [
+    //         "Django",
+    //         "Python",
+    //         "PostgreSQL",
+    //     ],
+    //     description: `
+    //         Back-end
+    //         (Privado)
+    //     `,
+    //     finishedAt: "02/12/2023",
+    //     linkGithub: "https://github.com/Willcley/m5-pet-kare-willcley",
+    //     publicGithub: true,
+    //     linkAppliaction: null,
+    //     online: false,
+    // },
+    {
+        img: null,
+        isNew: false,
+        title: "KImóveis",
+        type: "back-end",
+        kenzie: true,
+        framework: "Express",
+        techList: [
+            "Express",
+            "Typescript",
+            "Zod",
+            "TypeORM",
+            "PostgreSQL",
+            "Node.js",
+        ],
+        description: `
+            Esse foi um dos meus projetos de back-end, onde simula uma api de imobiliária.
+            \n
+            Aqui eu ja havia pouco mais de um mês de estudos sobre back-end com Express.
+            \n
+            Aprendi sobre relacionamentos de tabela de um pra muitos, e muitos pra muitos, usando TypeORM.
+        `,
+        finishedAt: "28/10/2023",
+        linkGithub: "https://github.com/Kenzie-Academy-Brasil-Developers/m4-kimoveis_willcley",
+        publicGithub: true,
+        linkAppliaction: null,
+        online: false,
+    },
+    {
+        img: "burguerKenzie.png",
+        isNew: false,
+        title: "Burguer Kenzie",
+        type: "front-end",
+        kenzie: true,
+        framework: "React.js",
+        techList: [
+            "React.js",
+            "Sass",
+            "Html",
+            "axios",
+        ],
+        description: `
+            Projeto que simula um site simples de lanchonete.
+            \n
+            Com ele aprendi sobre como fazer requisições HTTP no React, e tambem aprendi sobre axios e o uso de useEffect.
+            \n
+            Ele possui um modal de carrinho, com a lista de compras.
+        `,
+        finishedAt: "07/08/2023",
+        linkGithub: "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s3-template-hamburgueria_willcley",
+        publicGithub: true,
+        linkAppliaction: "https://react-entrega-s3-template-hamburgueria-willcley.vercel.app",
+        online: true,
+    },
+    {
+        img: "nuKenzie.png",
+        isNew: false,
+        title: "Nu Kenzie",
+        type: "front-end",
+        kenzie: true,
+        framework: "React.js",
+        techList: [
+            "React.js",
+            "Sass",
+            "Html",
+        ],
+        description: `
+            Esse projeto simula um site de banco.
+            \n
+            Onde aprendi sobre algumas coisas básicas de React.js, e conheci a linguagem de estilização que mais gosto (SASS).
+        `,
+        finishedAt: "01/08/2023",
+        linkGithub: "https://github.com/Kenzie-Academy-Brasil-Developers/react-entrega-s1-template-nu-kenzie_willcley",
+        publicGithub: true,
+        linkAppliaction: "https://react-entrega-s1-template-nu-kenzie-willcley.vercel.app",
+        online: true,
+    },
+    {
+        img: "openMusic.png",
+        isNew: false,
+        title: "Open Music",
+        type: "front-end",
+        kenzie: true,
+        framework: null,
+        techList: [
+            "Html",
+            "Css",
+            "Javascript",
+        ],
+        description: `
+            Um site que simula venda de músicas.
+            \n
+            Nele desenvolvi melhor meu conhecimento sobre filtragem de itens, e estilizações (como a barra de filtragem, e alternância para modo claro/escuro).
+        `,
+        finishedAt: "16/06/2023",
+        linkGithub: "https://github.com/Kenzie-Academy-Brasil-Developers/open-music-base_willcley",
+        publicGithub: true,
+        linkAppliaction: "https://kenzie-academy-brasil-developers.github.io/open-music-base_willcley/",
+        online: true,
+    },
+    {
+        img: "controlFinance.png",
+        isNew: false,
+        title: "Control Finance",
+        type: "front-end",
+        kenzie: true,
+        framework: null,
+        techList: [
+            "Html",
+            "Css",
+            "Javascript",
+        ],
+        description: `
+            Site que calcula transações bancárias.
+            \n
+            Projeto feito para praticar o uso de métodos e código limpo, em que aprendi alguns assuntos mais teóricos e de organização.
+        `,
+        finishedAt: "09/06/2023",
+        linkGithub: "https://github.com/Kenzie-Academy-Brasil-Developers/control-finance_willcley",
+        publicGithub: true,
+        linkAppliaction: "https://kenzie-academy-brasil-developers.github.io/control-finance_willcley/",
+        online: true,
+    },
+    {
+        img: "siteInstitucionalGenerico.png",
+        isNew: false,
+        title: "Site Institucional Genérico",
+        type: "front-end",
+        kenzie: true,
+        framework: null,
+        techList: [
+            "Html",
+            "Css",
+            "Javascript",
+        ],
+        description: `
+            Projeto que simula um template para sites institucionais.
+            \n
+            Nele aprendi melhor sobre boas práticas de código, e aprofundei mais meus conhecimentos de estilização usando CSS.
+            \n
+            Um site de fácil navegação, com layout simples e direto.
+        `,
+        finishedAt: "26/05/2023",
+        linkGithub: "https://github.com/Kenzie-Academy-Brasil-Developers/m2-criando-site-institucional-generico_willcley",
+        publicGithub: true,
+        linkAppliaction: "https://kenzie-academy-brasil-developers.github.io/m2-criando-site-institucional-generico_willcley/",
+        online: true,
+    },
+];
---
+++ src/databases/projectList.ts
@@ -0,0 +1,18 @@
+import { TProject } from "@/interfaces/projects.interfaces";
+
+export const projectList: TProject[] = [
+    // {
+    //     img: null,
+    //     isNew: false,
+    //     title: null,
+    //     kenzie: false,
+    //     techList: [
+    //     ],
+    //     description: null,
+    //     finishedAt: null,
+    //     linkGithub: null,
+    //     publicGithub: false,
+    //     linkAppliaction: null,
+    //     online: false
+    // },
+];
